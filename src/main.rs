06482fa903204dbe917d0be72d79d8772da50962
Benjamin Pazienza <pazienza.log@gmail.com>
2022-04-19 22:51:44 -0400

    First blog commits. Changing all URL code to blog code and expanding blog object

--- src/main.rs
+++ src/main.rs
@@ -21,13 +21,17 @@ use std::io::Cursor;
 
 
 #[derive(FromForm)]
-pub struct UrlBody {
-    url: String,
+pub struct BlogBody {
+    id: String,
+    title: String,
+    text: String,
+    story: String,
+    date: String,
 }
 
-fn get_url_collection() -> Collection {
+fn get_blog_collection() -> Collection {
     match Client::with_uri_str("mongodb://localhost:27017") {
-        Ok(client) => client.database("ienza-tech").collection("urls"),
+        Ok(client) => client.database("ienza-tech").collection("blogs"),
         Err(e) => {
             println!("Error while communicating with MONGODB{:?}", e);
             panic!()
@@ -57,43 +61,46 @@ fn get_bad_message() -> String {
     "The URL you requested is not in our database".to_string()
 }
 
-/// Given an ID get th
+/// Given an ID get the blog
 #[get("/<id>")]
-fn get_url(id: String) -> Redirect {
-    let collection = get_url_collection();
-    match collection.find_one(doc! { "id": id.clone()}, None) {
-        Ok(Some(result)) => match result.get_str("url") {
-            Ok(url) =>  get_redirect(url.to_string()),
-            Err(e) => {
-                println!("Database error: Found object in database but could not get URL: {:?}", e);
-                get_redirect("/errors/internal".to_string())
-            }
-        },
-        Ok(None) => get_redirect("/errors/bad".to_string()),
-        Err(e) => {
-            println!("Database error while trying to get the URL for the given id: {:?}", e);
-            get_redirect("/errors/internal".to_string())
-        }
-    }
+fn get_blog(id: String) -> Redirect {
+    // get a specific blog
 }
 
 #[get("/manage/all")]
 fn get_all() -> Response< 'static> {
     let mut vars = String::from("{");
-    let collection = get_url_collection();
+    vars.push_str(&"\"blogs\":[".to_string());
+    let collection = get_blog_collection();
     match collection.find(doc! {}, None) {
         Ok(cursor) => {
             for i in cursor {
               match i {
                 Ok(doc) => {
                     match doc.get_str("id") {
-                        Ok(id) => match doc.get_str("url") {
-                            Ok(url) => {
-                                vars.push_str(&"\"".to_string());
-                                vars.push_str(&id.to_string());
-                                vars.push_str(&"\":\"".to_string());
-                                vars.push_str(&url.to_string());
-                                vars.push_str(&"\",".to_string());
+                        Ok(id) => match doc.get_str("title") {
+                            Ok(title) => match doc.get_str("story") {
+                                Ok(story) => match doc.get_str("text") {
+                                    Ok(text) => match doc.get_str("date") {
+                                        Ok(date) => {
+                                            vars.push_str(&"{\"id\":\"".to_string());
+                                            vars.push_str(&id.to_string());
+                                            vars.push_str(&"\",\"title\":\"".to_string());
+                                            vars.push_str(&title.to_string());
+                                            vars.push_str(&"\",\"text\":\"".to_string());
+                                            vars.push_str(&text.to_string());
+                                            vars.push_str(&"\",\"story\":\"".to_string());
+                                            vars.push_str(&story.to_string());
+                                            vars.push_str(&"\",\"date\":\"".to_string());
+                                            vars.push_str(&date.to_string());
+                                            vars.push_str(&"\"".to_string());
+                                            vars.push_str(&"},".to_string());
+                                        },
+                                        Err(e) => println!("Error getting doc {:?}", e)
+                                    },
+                                    Err(e) => println!("Error getting doc {:?}", e)
+                                },
+                                Err(e) => println!("Error getting doc {:?}", e)
                             },
                             Err(e) => println!("Error getting doc {:?}", e)
                         },
@@ -109,64 +116,51 @@ fn get_all() -> Response< 'static> {
         }
     }
     vars.pop();
-    vars.push_str(&"}".to_string());
+    vars.push_str(&"]}".to_string());
     let mut response = Response::new();
     response.set_sized_body(Cursor::new(vars));
-    response.adjoin_raw_header("Access-Control-Allow-Origin", "https://manage.url.ienza.tech");
+    response.adjoin_raw_header("Access-Control-Allow-Origin", "https://blog.paz.ienza.tech");
     response.set_status(Status::Accepted);
     return response;
 }
 
 
-/// Add a url to the database (only if needed) and get the id of the object for the short url
+/// Add a blog to the database and get the id of the new blog
 #[post("/manage/add", data = "<body>")]
-fn add_url(body: Form<UrlBody>) -> String {
+fn add_blog(body: Form<BlogBody>) -> String {
+    let title = body.title.clone();
+    let story = body.story.clone();
+    let text = body.text.clone();
+    let date = body.date.clone();
+    let collection = get_blog_collection();
 
-    let url = body.url.clone();
-    let collection = get_url_collection();
-
-    match collection.find_one(doc! { "url": url.clone()}, None) {
-        Ok(Some(result)) => match result.get_str("id") {
-            Ok(id) => return id.to_string(),
-            Err(e) => {
-                println!("Database error while getting the ID that matches the URL: {:?}", e);
-                return "Internal Database Error".to_string();
-            }
-        },
-        Ok(None) => {
-            let mut n = 30;
-            while n > 0 {
-                let id = get_id();
-                match collection.find_one(doc! { "id": id.clone()}, None) {
-                    Ok(Some(_result)) => {
-                        n -= 1;
-                        continue;
-                    },
-                    Ok(None) => {
-                        match collection.insert_one(doc! {"url": url.clone(), "id": id.clone()}, None) {
-                            Ok(_result) => return format!("Your new url is: https://url.ienza.tech/{}", id),
-                            Err(e) => {
-                                println!("Database error while inserting key and URL to db: {:?}", e);
-                                return "Internal Database Error".to_string();
-                            }
-                        }
-                    },
+    let mut n = 30;
+    while n > 0 {
+        let id = get_id();
+        match collection.find_one(doc! { "id": id.clone()}, None) {
+            Ok(Some(_result)) => {
+                n -= 1;
+                continue;
+            },
+            Ok(None) => {
+                match collection.insert_one(doc! {"title": title.clone(), "id": id.clone(), "text": text.clone(), "story":story.clone(), "date": date.clone}, None) {
+                    Ok(_result) => return format!("Your new blog ID is: {}", id),
                     Err(e) => {
-                        println!("Server error while making sure key is unique: {:?}", e);
+                        println!("Database error while inserting blog to db: {:?}", e);
                         return "Internal Database Error".to_string();
                     }
                 }
+            },
+            Err(e) => {
+                println!("Server error while making sure key is unique: {:?}", e);
+                return "Internal Database Error".to_string();
             }
-            return "Unable to generate a unique key after 30 attemps".to_string();
-        }
-        Err(e) => {
-            println!("Database error while checking for existing key for the URL: {:?}", e);
-            return "Internal Database Error".to_string();
         }
     }
+    return "Unable to generate a unique key after 30 attemps".to_string();
 }
 
 /// Ignite the rocket and then sit patiently and wait while it crushes the game
 fn main() {
-    rocket::ignite().mount("/", routes![get_all, get_url, add_url, get_bad_message, get_internal_error_message]).launch();
+    rocket::ignite().mount("/", routes![get_all, get_blog, add_blog, get_bad_message, get_internal_error_message]).launch();
 }
